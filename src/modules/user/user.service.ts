f18e333e2641e0aae91ad92ae76e7c6cabdd661d
Khamzat91 <104260769+Khamzat91@users.noreply.github.com>
2024-01-10 23:41:23 +0300

    -Create Auth module, -Create methods login and register, -Create Jwt guard and jwt strategy

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,32 +1,50 @@
-import {BadRequestException, Injectable} from '@nestjs/common';
-import {InjectModel} from "@nestjs/sequelize";
-import {User} from "./modules/user.modules";
-import * as bcrypt from 'bcrypt'
-import {CreateUserDTO} from "./dto";
-import {AppError} from "../../common/constants/errors";
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from './modules/user.modules';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDTO, UpdateUserDTO } from './dto';
+import { AppError } from '../../common/constants/errors';
 
 @Injectable()
 export class UserService {
-    constructor(@InjectModel(User) private readonly userRepository: typeof User) {
-    }
+  constructor(
+    @InjectModel(User) private readonly userRepository: typeof User,
+  ) {}
 
-    async hashPassword(password) {
-        return bcrypt.hash(password, 10)
-    }
+  async hashPassword(password) {
+    return bcrypt.hash(password, 10);
+  }
 
-    async findUserByEmail(email: string) {
-        return this.userRepository.findOne({where: {email}})
-    }
+  async findUserByEmail(email: string) {
+    return this.userRepository.findOne({ where: { email } });
+  }
 
-    async createUser(dto: CreateUserDTO): Promise<CreateUserDTO> {
-        dto.password = await this.hashPassword(dto.password)
-        const newUser = {
-            firstName: dto.firstName,
-            username: dto.username,
-            email: dto.email,
-            password: dto.password
-        }
-        await this.userRepository.create(newUser)
-        return dto
-    }
+  async createUser(dto: CreateUserDTO): Promise<CreateUserDTO> {
+    dto.password = await this.hashPassword(dto.password);
+    const newUser = {
+      firstName: dto.firstName,
+      username: dto.username,
+      email: dto.email,
+      password: dto.password,
+    };
+    await this.userRepository.create(newUser);
+    return dto;
+  }
+
+  async publicUser(email: string) {
+    return this.userRepository.findOne({
+      where: { email },
+      attributes: { exclude: ['password'] },
+    });
+  }
+
+  async updateUser(email: string, dto: UpdateUserDTO): Promise<UpdateUserDTO> {
+    await this.userRepository.update(dto, { where: { email } });
+    return dto;
+  }
+
+  async deleteUser(email: string) {
+    await this.userRepository.destroy({ where: { email } });
+    return true;
+  }
 }
