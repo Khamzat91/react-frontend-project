10cfbebc753789111854c71f7d9b9bd8ff74071b
Khamzat91 <104260769+Khamzat91@users.noreply.github.com>
2024-01-15 11:12:09 +0300

    added  ApiTags and ApiResponse

--- src/modules/watchlist/watchlist.controller.ts
+++ src/modules/watchlist/watchlist.controller.ts
@@ -12,18 +12,24 @@ import {
 import { WatchlistService } from './watchlist.service';
 import { WatchlistDTO } from './dto';
 import { JwtAuthGuard } from '../../guards/jwt-quards';
+import { CreateAssetResponse } from "./response";
+import { ApiResponse, ApiTags } from "@nestjs/swagger";
 
 @Controller('watchlist')
 export class WatchlistController {
   constructor(private readonly watchlistService: WatchlistService) {}
 
+  @ApiTags("API")
+  @ApiResponse({status: 201, type: CreateAssetResponse})
   @UseGuards(JwtAuthGuard)
   @Post('create')
-  createAsset(@Body() assetDto: WatchlistDTO, @Req() request) {
+  createAsset(@Body() assetDto: WatchlistDTO, @Req() request):Promise<CreateAssetResponse> {
     const user = request.user;
     return this.watchlistService.createAsset(user, assetDto);
   }
 
+  @ApiTags("API")
+  @ApiResponse({status: 200})
   @UseGuards(JwtAuthGuard)
   @Delete()
   deleteAsset(@Query('id') assetId: string, @Req() request): Promise<boolean> {
